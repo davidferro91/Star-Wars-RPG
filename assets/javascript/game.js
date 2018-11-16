4f59038528d9710f97b839ff295c8013e5f7593e
davidferro <david-ferro@sbcglobal.net>
2018-11-16 16:43:57 -0500

    javascript generated images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,40 @@
-var hitpoints = [110, 140, 120, 170];
+var hitPoints = [110, 140, 120, 170];
 var attackValues = [10, 5, 7, 4];
 var counterAttack = [25, 40, 30, 55];
+var imageIdArray = ["vaderImg", "obiwanImg", "yodaImg", "jarjarImg"];
+var imageSourceArray = ["assets/images/darthVader.jpg", "assets/images/obiWan.jpg", "assets/images/yoda.jpg", "assets/images/darthJarJar.jpg"];
+var imageAltArray = ["Darth Vader", "ObiWan Kenobi", "Yoda", "Darth Jar Jar"]
+var topHolderArray = ["#vaderHolder", "#obiwanHolder", "#yodaHolder", "#jarjarHolder"];
+
+for (var i=0; i<hitPoints.length; i++) {
+    var image = createImage(i);
+    $(topHolderArray[i]).append(image);
+}
+
+function createImage (i) {
+    var imageHero = $("<img>");
+    imageHero.addClass("heroImage");
+    imageHero.attr("id", imageIdArray[i]);
+    imageHero.attr("src", imageSourceArray[i]);
+    imageHero.attr("alt", imageAltArray[i]);
+    imageHero.attr("data-hitPointValue", hitPoints[i]);
+    imageHero.attr("data-attackValue", attackValues[i]);
+    imageHero.attr("data-counterAttack", counterAttack[i]);
+    return imageHero;
+}
+
+$("#vaderImg").on("click", function() {
+    
+});
+
+$("#obiwanImg").on("click", function () {
+
+});
+
+$("#yodaImg").on("click", function () {
+
+});
+
+$("jarjarImg").on("click", function() {
+
+});
