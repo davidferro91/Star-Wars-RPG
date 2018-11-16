0f90e67a025fdf909bf67bb381bbe898de85b4e6
davidferro <david-ferro@sbcglobal.net>
2018-11-16 17:54:44 -0500

    clicking removes character Holders

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
-var hitPoints = [110, 140, 120, 170];
+var hitPoints = [140, 120, 110, 170];
 var attackValues = [10, 5, 7, 4];
 var counterAttack = [25, 40, 30, 55];
 var imageIdArray = ["vaderImg", "obiwanImg", "yodaImg", "jarjarImg"];
 var imageSourceArray = ["assets/images/darthVader.jpg", "assets/images/obiWan.jpg", "assets/images/yoda.jpg", "assets/images/darthJarJar.jpg"];
-var imageAltArray = ["Darth Vader", "ObiWan Kenobi", "Yoda", "Darth Jar Jar"]
+var imageAltArray = ["Darth Vader", "ObiWan Kenobi", "Yoda", "Darth Jar Jar Binks"]
 var topHolderArray = ["#vaderHolder", "#obiwanHolder", "#yodaHolder", "#jarjarHolder"];
+var charSelector = -1;
+var enemSelector = -1;
+var yourChar = false;
+var enemChar = false;
 
 for (var i=0; i<hitPoints.length; i++) {
     var image = createImage(i);
@@ -12,29 +16,46 @@ for (var i=0; i<hitPoints.length; i++) {
 }
 
 function createImage (i) {
+    var heroFigure=$("<figure>")
     var imageHero = $("<img>");
-    imageHero.addClass("heroImage");
-    imageHero.attr("id", imageIdArray[i]);
+    imageHero.addClass("heroImage figure-img m-0 rounded");
     imageHero.attr("src", imageSourceArray[i]);
     imageHero.attr("alt", imageAltArray[i]);
-    imageHero.attr("data-hitPointValue", hitPoints[i]);
-    imageHero.attr("data-attackValue", attackValues[i]);
-    imageHero.attr("data-counterAttack", counterAttack[i]);
-    return imageHero;
+    heroFigure.append(imageHero);
+    heroFigure.addClass("figure");
+    heroFigure.attr("id", imageIdArray[i]);
+    heroFigure.attr("data-hitPointValue", hitPoints[i]);
+    heroFigure.attr("data-attackValue", attackValues[i]);
+    heroFigure.attr("data-counterAttack", counterAttack[i]);
+    var heroCaption = $("<figcaption>");
+    heroCaption.addClass("figure-caption text-center text-primary captionBackground mt-1 rounded");
+    heroCaption.append(imageAltArray[i] + "<br>Hit Points: " + hitPoints[i]);
+    heroFigure.append(heroCaption);
+    return heroFigure;
+}
+
+function characterSelected (i) {
+    for (var j=0; j<hitPoints.length;j++) {
+        $(topHolderArray[j]).empty();
+    }
 }
 
 $("#vaderImg").on("click", function() {
-    
+    charSelector = 0;
+    characterSelected(charSelector);
 });
 
 $("#obiwanImg").on("click", function () {
-
+    charSelector = 1;
+    characterSelected(charSelector);
 });
 
 $("#yodaImg").on("click", function () {
-
+    charSelector = 2;
+    characterSelected(charSelector);
 });
 
-$("jarjarImg").on("click", function() {
-
+$("#jarjarImg").on("click", function() {
+    charSelector = 3;
+    characterSelected(charSelector);
 });
