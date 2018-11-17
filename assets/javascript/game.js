9c257e68ddc6dd463a27576596660426566ed91a
davidferro <david-ferro@sbcglobal.net>
2018-11-16 22:06:34 -0500

    can finally make a defender

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,34 @@ var attackValues = [10, 5, 7, 4];
 var counterAttack = [25, 40, 30, 55];
 var imageIdArray = ["vaderImg", "obiwanImg", "yodaImg", "jarjarImg"];
 var imageSourceArray = ["assets/images/darthVader.jpg", "assets/images/obiWan.jpg", "assets/images/yoda.jpg", "assets/images/darthJarJar.jpg"];
-var imageAltArray = ["Darth Vader", "ObiWan Kenobi", "Yoda", "Darth Jar Jar Binks"]
+var imageAltArray = ["Darth Vader", "ObiWan Kenobi", "Yoda", "Darth Jar Jar Binks"];
 var topHolderArray = ["#vaderHolder", "#obiwanHolder", "#yodaHolder", "#jarjarHolder"];
+var yourHolderArray = ["#yourVaderHolder", "#yourObiwanHolder", "#yourYodaHolder", "#yourJarjarHolder"];
+var enemHolderArray = ["#enemVaderHolder", "#enemObiwanHolder", "#enemYodaHolder", "#enemJarjarHolder"];
+var defHolderArray = ["#defVaderHolder", "#defObiwanHolder", "#defYodaHolder", "#defJarjarHolder"];
 var charSelector = -1;
 var enemSelector = -1;
 var yourChar = false;
 var enemChar = false;
 
 for (var i=0; i<hitPoints.length; i++) {
-    var image = createImage(i);
-    $(topHolderArray[i]).append(image);
+    var topImage = createImage(i);
+    $(topHolderArray[i]).append(topImage);
+    var yourImage = createImage(i);
+    $(yourHolderArray[i]).append(yourImage);
+    $(yourHolderArray[i]).hide();
+    var enemImage = createImage(i);
+    enemImage.attr("id", imageIdArray[i]+"2");
+    $(enemHolderArray[i]).append(enemImage);
+    $(enemHolderArray[i]).hide();
+    var defImage = createImage(i);
+    $(defHolderArray[i]).append(defImage);
+    $(defHolderArray[i]).hide();
 }
+$("#restBtn").hide();
 
 function createImage (i) {
-    var heroFigure=$("<figure>")
+    var heroFigure = $("<figure>")
     var imageHero = $("<img>");
     imageHero.addClass("heroImage figure-img m-0 rounded");
     imageHero.attr("src", imageSourceArray[i]);
@@ -36,26 +50,73 @@ function createImage (i) {
 
 function characterSelected (i) {
     for (var j=0; j<hitPoints.length;j++) {
-        $(topHolderArray[j]).empty();
+        $(topHolderArray[j]).hide();
+    }
+    yourChar=true;
+    $(yourHolderArray[i]).show();
+    for (var j=1;j<hitPoints.length;j++) {
+        $(enemHolderArray[(i+j)%4]).show();
     }
 }
 
+function enemySelected (i) {
+    $(enemHolderArray[i]).hide();
+    enemChar=true;
+    $(defHolderArray[i]).show();
+}
+
 $("#vaderImg").on("click", function() {
-    charSelector = 0;
-    characterSelected(charSelector);
+    if (yourChar == false) {
+        charSelector = 0;
+        characterSelected(charSelector);
+    }
 });
 
 $("#obiwanImg").on("click", function () {
-    charSelector = 1;
-    characterSelected(charSelector);
+    if (yourChar == false) {
+        charSelector = 1;
+        characterSelected(charSelector);
+    }
 });
 
 $("#yodaImg").on("click", function () {
-    charSelector = 2;
-    characterSelected(charSelector);
+    if (yourChar == false) {
+        charSelector = 2;
+        characterSelected(charSelector);
+    }
 });
 
 $("#jarjarImg").on("click", function() {
-    charSelector = 3;
-    characterSelected(charSelector);
+    if (yourChar == false) {
+        charSelector = 3;
+        characterSelected(charSelector);
+    }
+});
+
+$("#vaderImg2").on("click", function () {
+    if (charSelector != 0 && enemChar == false) {
+        enemSelector = 0;
+        enemySelected(enemSelector);
+    }
+});
+
+$("#obiwanImg2").on("click", function(){
+    if (charSelector != 1 && enemChar == false) {
+        enemSelector = 1;
+        enemySelected(enemSelector);
+    }
+});
+
+$("#yodaImg2").on("click", function () {
+    if (charSelector != 2 && enemChar == false) {
+        enemSelector = 2;
+        enemySelected(enemSelector);
+    }
+});
+
+$("#jarjarImg2").on("click", function(){
+    if (charSelector != 3 && enemChar == false) {
+        enemSelector = 3;
+        enemySelected(enemSelector);
+    }
 });
